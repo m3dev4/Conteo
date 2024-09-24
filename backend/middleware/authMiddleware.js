f7fa573e2cae9753eebd2c16386334fbfef83c5c
m3dev4 <m3dev4@gmail.com>
2024-09-24 19:45:45 +0000

    commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "./asyncHandler.js";
-import User from "../models/UserModel.js";
+import User from "../models/userModel.js";
 
 // Middleware d'authentification
 const authenticate = asyncHandler(async (req, res, next) => {
